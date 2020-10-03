cb99c0ed309d7cb50e477a644b6ef5048a463b96
masa5555 <masa57887@gmail.com>
2020-10-04 03:54:26 +0900

    エラーメッセージで位置を指摘するように改良

--- 9cc.c
+++ 9cc.c
@@ -25,11 +25,30 @@ struct Token {
 // 現在着目しているtoken
 Token *token;
 
+char *user_input;
+
 // エラー報告のための関数    
 // printfと同じ引数をとる
+// "..."は可変長引数
 void error(char *fmt, ...){
   va_list ap;
   va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// エラー箇所を表示する
+void error_at(char *loc, char *fmt, ...){
+  va_list ap;
+  va_start(ap, fmt);
+
+  // 変更部分
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, ""); //空白で列合わせる
+  fprintf(stderr, "^ "); 
+
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
@@ -50,7 +69,7 @@ bool consume(char op){
 // それ以外ならば、エラーを報告する。
 void expect(char op){
   if(token->kind != TK_RESERVED || token->str[0] != op){
-    error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   }
   token = token->next;
 }
@@ -59,7 +78,7 @@ void expect(char op){
 // それ以外ならば、エラーを報告する。
 int expect_number(){
   if(token->kind != TK_NUM){
-    error("数値ではありません");
+    error_at(token->str, "数値ではありません");
   }
   int val = token->val;
   token = token->next;
@@ -80,7 +99,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str){
 }
 
 // 入力文字列pをtokenizeしたものを返す
-Token *tokenize(char *p){
+Token *tokenize(){
+  char *p = user_input;
   Token head;
   head.next = NULL;
   Token *cur = &head;
@@ -103,7 +123,7 @@ Token *tokenize(char *p){
       continue;
     }
 
-    error("tokenizeできません");
+    error_at(p, "tokenizeできません");
   }
 
   new_token(TK_EOF, cur, p);
@@ -117,7 +137,8 @@ int main(int argc, char **argv){
   }
 
   // tokenizeし、連結リストを作る
-  token = tokenize(argv[1]);
+  user_input = argv[1];
+  token = tokenize();
   
   // アセンブラのヘッダー部分
   printf(".intel_syntax noprefix\n");
